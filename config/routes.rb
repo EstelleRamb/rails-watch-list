819d986bdf5c3832669d7063efefa98729cbc442
estelle rambur <estelle.rambur@gmail.com>
2022-11-24 16:40:27 -0500

    a user can delete a bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
- resources :lists, only: %i[index create show new]
+ resources :lists, only: %i[index create show new] do
+  resources :bookmarks, only: %i[create new]
+ end
+ resources :bookmarks, only: %i[destroy]
 
 end
